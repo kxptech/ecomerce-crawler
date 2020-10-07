5aff237914dae369029cb2d033c4f4b54be30294
montsouz <guilhermesouzamont@gmail.com>
2020-10-06 21:28:13 -0300

    feat: adding mercado_livre crawler

--- src/crawlers/mercado_livre.py
+++ src/crawlers/mercado_livre.py
@@ -1,1 +1,22 @@
-print('this is going to be a crawler for Mercado Livre')
+from bs4 import BeautifulSoup
+import requests
+
+res = requests.get('https://lista.mercadolivre.com.br/botas')
+soup = BeautifulSoup(res.text, 'html.parser')
+items = soup.find_all('div', {'class': 'ui-search-result__content-wrapper'})
+
+for i in range(len(items)):
+    item_to_be_parsed = BeautifulSoup(str(items[i]), 'html.parser')
+    title = item_to_be_parsed.find(
+        'h2', 'ui-search-item__title').text
+    price_fraction = item_to_be_parsed.find('span', 'price-tag-fraction').text
+
+    try:
+        price_cents = item_to_be_parsed.find(
+            'span', 'price-tag-cents').text
+    except:
+        price_cents = 0
+
+    full_price = 'R$ {},{}'.format(price_fraction, price_cents)
+
+    print(title, full_price)
