2c736011caf45520af6e98abfb6ed887b77d4f44
bruno-augusto-pinto <brunoaugustopinto@hotmail.com>
2020-10-25 18:13:16 -0300

    Feature: added CSV file writer.

--- src/crawlers/mercado_livre.py
+++ src/crawlers/mercado_livre.py
@@ -2,41 +2,39 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-aux = 1
+aux = 0
+csv_exit = []
 
-site = []
+with open("Arquivo\\Entrada.csv", "r") as arquivo_csv:
+    csv_items = csv.reader(arquivo_csv, delimiter=',')
+    for item in csv_items:
+        item = '-'.join(item)
+        item = str(item).replace(' ', '-')
+        item = 'https://lista.mercadolivre.com.br/' + item
+        res = requests.get(item)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        items = soup.find_all('div', {'class': 'ui-search-result__content-wrapper'})
 
-with open("Entrada.csv", "r") as arquivo_csv:
-    leitor = csv.reader(arquivo_csv, delimiter=',')
-    for coluna in leitor:
-        palavras = str(coluna).split()
-        for x in palavras:
-            if aux == 1:
-                y = 'https://lista.mercadolivre.com.br/' + x
-                aux = aux + 1
-            else:
-                y = y + "-" + x
-                aux = aux + 1
-        site.append(y)
-        aux = 1
-print(site)
+        csv_exit.append('Saída' + str(aux) + '.csv')
+        with open(csv_exit[aux], 'w', newline='') as file:
 
-res = requests.get('https://lista.mercadolivre.com.br/botas')
-soup = BeautifulSoup(res.text, 'html.parser')
-items = soup.find_all('div', {'class': 'ui-search-result__content-wrapper'})
+            writer = csv.writer(file)
 
-for i in range(len(items)):
-    item_to_be_parsed = BeautifulSoup(str(items[i]), 'html.parser')
-    title = item_to_be_parsed.find(
-        'h2', 'ui-search-item__title').text
-    price_fraction = item_to_be_parsed.find('span', 'price-tag-fraction').text
+            writer.writerow(["Produto", "Valor"])
 
-    try:
-        price_cents = item_to_be_parsed.find(
-            'span', 'price-tag-cents').text
-    except:
-        price_cents = 0
+            for i in range(len(items)):
+                item_to_be_parsed = BeautifulSoup(str(items[i]), 'html.parser')
+                title = item_to_be_parsed.find(
+                    'h2', 'ui-search-item__title').text
+                price_fraction = item_to_be_parsed.find('span', 'price-tag-fraction').text
 
-    full_price = 'R$ {},{}'.format(price_fraction, price_cents)
+                try:
+                    price_cents = item_to_be_parsed.find(
+                        'span', 'price-tag-cents').text
+                except:
+                    price_cents = 0
 
-    # print(title, full_price)
+                full_price = 'R$ {},{}'.format(price_fraction, price_cents)
+                writer.writerow([title, full_price])
+
+            aux = aux + 1
