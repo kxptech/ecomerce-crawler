066499f0b118912028025d651b8233ae53ade1a3
bruno-augusto-pinto <brunoaugustopinto@hotmail.com>
2020-10-27 23:40:09 -0300

    Fix: fixed bug when writing csv files

--- src/crawlers/csv_handler.py
+++ src/crawlers/csv_handler.py
@@ -4,12 +4,16 @@ import csv
 def csv_reader():
     with open("../../archives/Entrada.CSV", "r") as csv_file:
         csv_items = csv.reader(csv_file, delimiter=',')
-    return csv_items
+        items = []
+        for item in csv_items:
+            items.append(item)
+        return items
 
 
 def csv_writer(items, products):
-    for i in range(items):
-        with open('Saída ' + str(i + 1), 'w', newline='') as csv_file:
+    for i in range(len(items)):
+        with open('Saída ' + str(i + 1) + '.csv', 'w', newline='') as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerow(["PRODUTO", "VALOR"])
+            if i == 0:
+                writer.writerow(["PRODUTO", "VALOR"])
             writer.writerow(products[i])
--- src/crawlers/mercado_livre.py
+++ src/crawlers/mercado_livre.py
@@ -1,40 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
-import csv
+import csv_handler
 
-aux = 0
-csv_exit = []
+csv_items = csv_handler.csv_reader()
 
-with open("../../archives/Entrada.CSV", "r") as arquivo_csv:
-    csv_items = csv.reader(arquivo_csv, delimiter=',')
-    for item in csv_items:
-        item = '-'.join(item)
-        item = str(item).replace(' ', '-')
-        item = 'https://lista.mercadolivre.com.br/' + item
-        res = requests.get(item)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        items = soup.find_all('div', {'class': 'ui-search-result__content-wrapper'})
+for item in csv_items:
+    item = '-'.join(item)
+    item = str(item).replace(' ', '-')
+    item = 'https://lista.mercadolivre.com.br/' + item
+    res = requests.get(item)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    items = soup.find_all('div', {'class': 'ui-search-result__content-wrapper'})
+    products = []
+    for i in range(len(items)):
+        item_to_be_parsed = BeautifulSoup(str(items[i]), 'html.parser')
+        title = item_to_be_parsed.find(
+            'h2', 'ui-search-item__title').text
+        price_fraction = item_to_be_parsed.find('span', 'price-tag-fraction').text
 
-        csv_exit.append('Saída' + str(aux) + '.csv')
-        with open(csv_exit[aux], 'w', newline='') as file:
+        try:
+            price_cents = item_to_be_parsed.find(
+                'span', 'price-tag-cents').text
+        except:
+            price_cents = 0
 
-            writer = csv.writer(file)
+        full_price = 'R$ {},{}'.format(price_fraction, price_cents)
 
-            writer.writerow(["Produto", "Valor"])
+        products.append([title, full_price])
 
-            for i in range(len(items)):
-                item_to_be_parsed = BeautifulSoup(str(items[i]), 'html.parser')
-                title = item_to_be_parsed.find(
-                    'h2', 'ui-search-item__title').text
-                price_fraction = item_to_be_parsed.find('span', 'price-tag-fraction').text
-
-                try:
-                    price_cents = item_to_be_parsed.find(
-                        'span', 'price-tag-cents').text
-                except:
-                    price_cents = 0
-
-                full_price = 'R$ {},{}'.format(price_fraction, price_cents)
-                writer.writerow([title, full_price])
-
-            aux = aux + 1
+    csv_handler.csv_writer(csv_items, products)
