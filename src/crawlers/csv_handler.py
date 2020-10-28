c856f8791cb931adaea33d1bd9c813336ed5d1b3
bruno-augusto-pinto <brunoaugustopinto@hotmail.com>
2020-10-27 22:50:04 -0300

    Feature: Added separeted module for CSV files handler

--- src/crawlers/csv_handler.py
+++ src/crawlers/csv_handler.py
@@ -1,4 +1,15 @@
-def csv_reader():
-    with open("../../archives/Entrada.CSV", "r") as readed_csv_file:
-        csv_items = csv.reader(readed_csv_file, delimiter=',')
+import csv
 
+
+def csv_reader():
+    with open("../../archives/Entrada.CSV", "r") as csv_file:
+        csv_items = csv.reader(csv_file, delimiter=',')
+    return csv_items
+
+
+def csv_writer(items, products):
+    for i in range(items):
+        with open('Saída ' + str(i + 1), 'w', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(["PRODUTO", "VALOR"])
+            writer.writerow(products[i])
