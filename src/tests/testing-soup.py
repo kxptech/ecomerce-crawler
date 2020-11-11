3c74be72a7f8e13be2320969049c4ee1d0adadcd
bruno-augusto-pinto <brunoaugustopinto@hotmail.com>
2020-11-10 23:51:55 -0300

    First commit

---
+++ src/tests/testing-soup.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import requests
+from src.modules import csv_handler
+
+res = requests.get('https://lista.mercadolivre.com.br/botas')
+soup = BeautifulSoup(res.text, 'html.parser')
+
+items = soup.find_all('div', {'class': 'andes-card andes-card--flat andes-card--default ui-search-result ui-search-result--core andes-card--padding-default andes-card--animated'})
+
+for i in range(len(items)):
+    # print(items[i])
+    for link in soup.find_all('a', {'class': 'ui-search-link'}):
+        print(link.get('href'))
+
+#     for a in soup.find_all('a', href=True, text=True):
+#         link_text = a['href']
+#     print('Link: ' + str(i) + ' ' + link_text)
+
+# for aux in items:
+#     aux2 = aux.find_all('a', href=True, text=True)
+#     # link = aux2['href']
+#     print(aux2)
+
+
+# for i in range(len(items)):
+#     for a in soup.find_all('a', href=True, text=True):
+#         link_text = a['href']
+#     print('Link: ' + str(i) + ' ' + link_text)
+
+# for i in range(len(items)):
+#     soup = BeautifulSoup(str(items[i]), 'html.parser')
+#     link_text = ''
+#     for a in soup.find_all('a', href=True, text=True):
+#         link_text = a['href']
+#
+#     print('Link: ' + str(i) + ' ' + link_text)
+
+# links = soup.find_all('a', href=True)
+#
+# print(links)
+
+# ui-search-link
