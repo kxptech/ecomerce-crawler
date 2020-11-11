fc31595120b8a37a6137140dd164710597f10d16
bruno-augusto-pinto <brunoaugustopinto@hotmail.com>
2020-11-11 00:31:47 -0300

    Testing successfully, need integration

--- src/tests/testing-soup.py
+++ src/tests/testing-soup.py
@@ -5,12 +5,16 @@ from src.modules import csv_handler
 res = requests.get('https://lista.mercadolivre.com.br/botas')
 soup = BeautifulSoup(res.text, 'html.parser')
 
-items = soup.find_all('div', {'class': 'andes-card andes-card--flat andes-card--default ui-search-result ui-search-result--core andes-card--padding-default andes-card--animated'})
+# items = soup.find_all('div', {'class': 'andes-card andes-card--flat andes-card--default ui-search-result ui-search-result--core andes-card--padding-default andes-card--animated'})
+
+items = soup.find_all('div', {'class': 'ui-search-result__image'})
 
 for i in range(len(items)):
-    # print(items[i])
     for link in soup.find_all('a', {'class': 'ui-search-link'}):
-        print(link.get('href'))
+        if (str(link.get('title')) != 'None') and ('ui-search-result__content' in link['class']):
+            # print(link.attrs)
+            print(link.get('title'))
+            print(link.get('href'))
 
 #     for a in soup.find_all('a', href=True, text=True):
 #         link_text = a['href']
